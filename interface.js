e8f89c830bdf915751e348918e601c0489ad6a5f
antiz <oloturia@gmail.com>
2016-09-08 18:16:07 +0200

    Now mouse is tracked with a pygame panel

--- interface.js
+++ interface.js
@@ -2,16 +2,16 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/jquery', function(req,res){
-	res.sendFile(__dirname+'/jquery-1.11.1.js');
+app.get('/jquery', function(req,res){			//only if jquery is
+	res.sendFile(__dirname+'/jquery-1.11.1.js');	//stored locally
 });
 
 app.get('/',function(req,res){
-	res.sendFile(__dirname+'/interface.html');
+	res.sendFile(__dirname+'/interface.html');	//our interface
 });
 
 io.on('connection', function(socket){
-	socket.on('mousePositionChanged',function(msg){
+	socket.on('mousePositionChanged',function(msg){ //output in stdout
 		console.log(msg);
 	});
 });
--- receiver.py
+++ receiver.py
@@ -3,21 +3,25 @@ import json
 import signal
 import subprocess
 import sys
+import pygame
 
 def exit_handler(signal, frame):
 	print('\nExiting...')
 	node_script.terminate()
+        pygame.quit()
 	sys.exit(0)
 
 if __name__ == '__main__':
+        pygame.init()                                           #pygame init
+        screen = pygame.display.set_mode((500,500))             #same dimension as the canvas
 	signal.signal(signal.SIGINT, exit_handler)
-	node_script = subprocess.Popen(["node","interface.js"],stdout=subprocess.PIPE)
+	node_script = subprocess.Popen(["node","interface.js"],stdout=subprocess.PIPE) #launch nodejs
 	while True:
 		try:
-			output = json.loads(node_script.stdout.readline())
-			print output['x']
-			print output['y']
+			output = json.loads(node_script.stdout.readline())             #obtain a dict from JSON
+			screen.fill((255,0,0),(output['x'],output['y'],1,1))
+                        #print output['x']
+			#print output['y']
+                        pygame.display.update()
 		except ValueError:
-			not_json = node_script.stdout.readline()
-			if not_json != "":
-				print not_json
+			node_script.stdout.readline()           #discard unmeaningful data
