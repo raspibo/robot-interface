ea1ed0627988a48e7e9d236c6eda3936facc0a70
antiz <oloturia@gmail.com>
2016-09-12 01:57:14 +0200

    enabled joystick support

--- interface.js
+++ interface.js
@@ -6,8 +6,16 @@ app.get('/jquery', function(req,res){			//only if jquery is
 	res.sendFile(__dirname+'/jquery-1.11.1.js');	//stored locally
 });
 
-app.get('/',function(req,res){
-	res.sendFile(__dirname+'/interface.html');	//our interface
+app.get('/mouse.html',function(req,res){
+	res.sendFile(__dirname+'/mouse.html');	//mouse reader
+});
+
+app.get('/joystick.html',function(req,res){
+	res.sendFile(__dirname+'/joystick.html');
+});
+
+app.get('/*',function(req,res){
+	res.sendFile(__dirname+'/index.html');
 });
 
 io.on('connection', function(socket){
