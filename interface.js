8197704c50475f710a7849bf65f3bbf1922d5df3
oloturia <oloturia@gmail.com>
2016-09-19 00:48:29 +0000

    minor fixes

--- interface.js
+++ interface.js
@@ -2,10 +2,6 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/jquery', function(req,res){			//only if jquery is
-	res.sendFile(__dirname+'/jquery-1.11.1.js');	//stored locally
-});
-
 app.get('/mouse.html',function(req,res){
 	res.sendFile(__dirname+'/mouse.html');	//mouse reader
 });
